e99d661b3e0ec9c9b950f95acb9a2518361cd2d8
João <joao.melo@sptech.school>
2025-04-15 22:53:58 -0300

    [FEAT]: Added Cart page with cartCard component and form to send email

---
+++ src/components/cartCard/CartCard.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import TrashIcon from '@mui/icons-material/DeleteForever';
+import "./CartCard.css"; // Importa o arquivo CSS
+
+
+export default function CartCard(props) {
+    const [quantidade, setQuantidade] = useState(1);
+    const { nome, descricao, preco } = props;
+    return (
+        <div className="card-container">
+            <div className="card-header">
+                <TrashIcon className="trash-icon" sx={{ fontSize: '40px' }} />
+                <div className="product-details">
+                    <img
+                        src="https://www.automataweb.com.br/wp-content/uploads/2019/01/DSCN4860_Rev02-1024x728.jpg"
+                        alt="Product"
+                        className="product-image"
+                    />
+                    <div className="product-info">
+                        <div>
+                            <h3 className="product-name">{nome}</h3>
+                            <p className="product-description">{descricao}</p>
+                        </div>
+                        <p className="product-price">R$ {preco}</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="quantity-control">
+                <p className="quantity-label">Quantidade</p>
+                <div className="quantity-buttons">
+                    <button className="quantity-button" onClick={() => {
+                        if (quantidade > 0) {
+                            setQuantidade(quantidade - 1);
+                        }
+                    }}>-</button>
+                    <span className="quantity-value">{quantidade}</span>
+                    <button className="quantity-button" onClick={() => setQuantidade(quantidade + 1)}>+</button>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import LandingPage from '../pages/LandingPage.jsx'
+import CartPage from '../pages/CartPage/CartPage.jsx'
 import { Routes, Route } from 'react-router-dom'
 
 function AppRoutes() {
   return (
     <Routes>
         <Route path="/" element={<LandingPage/>} />
+        <Route path="/cart" element={<CartPage/>} />
     </Routes>
   )
 }
