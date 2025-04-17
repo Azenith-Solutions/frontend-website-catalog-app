0f56c8339df6b38c1557bfd36f4c297007e57e25
João <joao.melo@sptech.school>
2025-04-17 20:22:36 -0300

    [FEAT] :: Added Service Page

---
+++ src/pages/servicePage/ServicePage.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import NavBarCatalog from "../../components/navBar/NavBarCatalog"
+import Footer from "../../components/Footer"
+import Container from '@mui/material/Container'
+import CardService from '../../components/cards/CardService'
+import CardCarousel from '../../components/cardCarousel/CarouselCard'
+import './ServicePage.css'
+
+import { Email } from '@mui/icons-material';
+
+
+export default function ServicePage() {
+    return (
+        <>
+            <NavBarCatalog
+                menuItems={['Home', 'Serviços', 'Relacionados']}
+                redirectButtonName='Componentes'
+            />
+            <Container sx={{
+                maxWidth: { lg: '1600px' },
+                marginTop: { xs: '120px', md: '200px' },
+            }}>
+                <section className="image-text-section">
+                    <div className="image-container">
+                        <img
+                            src="https://www.automataweb.com.br/wp-content/uploads/2019/01/DSCN4860_Rev02-1024x728.jpg"
+                            alt="Service illustration"
+                        />
+                    </div>
+                    <div className="text-container">
+                        <h1>Serviço Especial</h1>
+                        <h2>Introdução</h2>
+                        <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    </div>
+                </section>
+
+                <section className="image-text-section reverse">
+                    <div className="image-container">
+                        <img
+                            src="https://www.automataweb.com.br/wp-content/uploads/2019/01/DSCN4860_Rev02-1024x728.jpg"
+                            alt="Service illustration"
+                        />
+                    </div>
+                    <div className="text-container">
+                        <h1>Negociação</h1>
+                        <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        <button className="cta-button">
+                            <Email sx={{ fontSize: '30px' }} />
+                            Solicitar Orçamento
+                        </button>
+                    </div>
+                </section>
+                
+                <section className='carousel-container'>
+                    <h1>Serviços Realizados</h1>
+                    <CardCarousel
+                        CardComponent={CardService}
+                        filter='processadores'
+                        uriEndPoint='http://localhost:8080'
+                    />
+                </section>
+            </Container>
+            <Footer
+                footerItems={['Home', 'Serviços', 'Relacionados']}
+            />
+        </>
+    )
+}
