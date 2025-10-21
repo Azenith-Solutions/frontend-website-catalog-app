66c9ec87abdb53a668e5fec5f0571f279f4d89d6
João Victor <joao.melo@sptech.school>
2025-10-20 22:41:46 -0300

    Ajustando endpoints para atender aos endpoints do clean archtecture

--- src/services/componentService.js
+++ src/services/componentService.js
@@ -4,7 +4,7 @@ export async function getComponent(filters) {
     console.log("Service foi chamada e iniciou a recuperação de componentes");
 
     try {
-        const response = await apiClient.get(`/components/catalog${filters}`);
+        const response = await apiClient.get(`/eletronic-components/catalog${filters}`);
         console.log("Resposta: ", response.data);
         return response.data;
     } catch (error) {
@@ -18,7 +18,7 @@ export async function getWhereComponentFilter(filters) {
     console.log("Service foi chamada e iniciou a recuperação de componentes");
 
     try {
-        const response = await apiClient.post(`/components/filterComponentList`, filters);
+        const response = await apiClient.post(`/eletronic-components/filterComponentList`, filters);
         console.log("Resposta WHERE: ", response.data);
         return response.data;
     } catch (error) {
@@ -32,7 +32,7 @@ export async function getComponentById(id) {
     console.log("Service foi chamada e iniciou a recuperação do componente");
 
     try {
-        const response = await apiClient.get(`/components/details/${id}`);
+        const response = await apiClient.get(`/eletronic-components/details/${id}`);
         console.log("Resposta componente by id: ", response.data);
         return response.data;
     } catch (error) {
